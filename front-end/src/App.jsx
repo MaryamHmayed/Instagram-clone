771a1d81350c18c943292be8dc4234228ea78dc7
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-04-08 03:32:57 +0300

    Routing login and register

---
+++ front-end/src/App.jsx
@@ -0,0 +1,28 @@
+import "./styles/utilities.css";
+import "./styles/colors.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <BrowserRouter>
+     <Routes>
+      <Route path="/"  element={<Login/>}/>
+      <Route path="/signup"  element={<Register/>}/>
+
+
+
+
+     </Routes>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
